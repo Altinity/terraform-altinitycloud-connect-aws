311404a063015081a74aae379336e0080e679d6b
Ignacio Anaya <ignacio.anaya89@gmail.com>
2026-03-24 21:46:44 -0300

    fix: add versions.tf with provider and terraform constraints
    Pin minimum versions for aws (>= 5.0), random (>= 3.0), tls (>= 4.0),
    and http (>= 3.0) providers. Require Terraform >= 1.0.

---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 3.0"
+    }
+  }
+}
